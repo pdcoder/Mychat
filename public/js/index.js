373954a1c05de1f12347946cc6b3f2b1ffad426c
Prakash <prakashdas1998.pd@gmail.com>
2018-06-17 00:49:28 +0530

    jj

--- public/js/index.js
+++ public/js/index.js
@@ -3,11 +3,9 @@ var socket = io();
             console.log('Connected to server');
 
         });
-
-        socket.on('disconnect',function(){
-            console.log("Disconnected from server");
-        });
-
         socket.on('newMessage',function(message){
             console.log('newMessage', message);
+        });
+        socket.on('disconnect',function(){
+            console.log("Disconnected from server");
         });
