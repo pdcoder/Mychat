2326d4284417f8b61c5cc1150d4c5084f81830f5
Prakash <prakashdas1998.pd@gmail.com>
2018-06-17 01:04:19 +0530

    jj

--- server/server.js
+++ server/server.js
@@ -12,6 +12,18 @@ var io = socketIO(server);
 io.on('connection',(socket)=>{
 console.log('New user connected');
 
+    socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app',
+    createdAt: new Date().getTime()
+    });
+
+    socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+    });
+    
     socket.on('createMessage',(message)=>{
     console.log('createMessage',message);
         io.emit('newMessage',{
